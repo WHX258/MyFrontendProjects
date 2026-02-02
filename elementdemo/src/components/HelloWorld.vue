a2e17eacf44800f84b8822d84fca4385887b3f8d
WHX258 <1225438764@qq.com>
2026-02-02 16:07:16 +0800

    main

--- elementdemo/src/components/HelloWorld.vue
+++ elementdemo/src/components/HelloWorld.vue
@@ -1,6 +1,22 @@
 <template>
+  <!--
+  :data绑定表格数据
+  el-table-column定义表格列
+  prop是tableData的key
+  label是显示给用户的文字
+  width是列的宽度
+
+  stripe属性使表格带有斑马纹
+  border属性使表格有垂直边框
+  :row-class-name为某一行添加 class，表明该行处于某种状态
+  在方法里定义tableRowClassName，根据rowIndex返回不同的class名称
+  函数中的row参数表示行数据，rowIndex表示行索引，比如可以使用row.name
+  -->
   <el-table
       :data="tableData"
+      stripe
+      border
+      :row-class-name="tableRowClassName"
       style="width: 100%">
     <el-table-column
         prop="date"
@@ -21,6 +37,16 @@
 
 <script>
 export default {
+  methods: {
+    tableRowClassName({row, rowIndex}) {
+      if (rowIndex === 0) {
+        return 'warning-row';
+      } else if (rowIndex === 2) {
+        return 'success-row';
+      }
+      return '';
+    }
+  },
   data() {
     return {
       tableData: [{
@@ -44,3 +70,13 @@ export default {
   }
 }
 </script>
+
+<style>
+.el-table .warning-row {
+  background: oldlace;
+}
+
+.el-table .success-row {
+  background: #f0f9eb;
+}
+</style>
